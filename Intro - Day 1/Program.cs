6fc91480651bc7886ca050ec05a10a5fdaa25ce3
Mj <developersholotan@gmail.com>
2024-06-27 11:44:18 +0100

    Low Battery

--- Intro - Day 1/Program.cs
+++ Intro - Day 1/Program.cs
@@ -4,8 +4,28 @@
     {
         static void Main(string[] args)
         {
-            
+           
             Console.WriteLine("Hello, World!");
+        }
+        
+       
+    }
+
+
+    public class MJay
+    {
+        public static string Name { get; set; }
+        private protected static void GreetMj()
+        {
+            Console.WriteLine("Hello There,  Mjay!");
+        }
+    }
+
+    public class NewMjay: MJay
+    {
+        public void JustaFunc()
+        {
+            GreetMj();  
         }
     }
 }
