09c162ddb7ae54d4618eb767abb3b6d4fb2e52ec
USER <USER@GAMER>
2024-06-27 10:43:38 +0100

    Add project files.

---
+++ Intro - Day 1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Intro___Day_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
