43b701e592df356489d23b1c66504fb879a67fd5
oluwapelumi11 <oluwapelumiezeliel11@gmail.com>
2024-06-27 12:47:19 +0100

    created classes

---
+++ Intro - Day 1/Pet.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Intro___Day_1
+{
+
+    public class Dog
+    {
+        public string name;
+
+        public string Name
+        {
+            get
+            {
+                return $" Pet name is {name}";
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public Dog(string name)
+        {
+            this.name = name;
+        }
+
+        public void MakeSound()
+        {
+            Console.WriteLine("I'm making a sound");
+        }
+    }
+
+    public class Cat
+    {
+        public string newname;
+        public string Newname
+        {
+            get
+            {
+                return $"Pet New name is {newname}";
+            }
+            set
+            {
+                newname = value;
+            }
+        }
+        public Cat(string newname)
+        {
+            this.newname = newname;
+        }
+        public void lives()
+        {
+            Console.WriteLine("I have 9 lives");
+        }
+    }
+}
