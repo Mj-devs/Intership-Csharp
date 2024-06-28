6c6ca1921a11b9f52460aab4635d20ad1e6d418d
Oluwapelumi Sunday <oluwapelumiezekiel11@gmail.com>
2024-06-28 10:35:37 +0100

    added data types and conversion

--- Intro - Day 1/Data Types/numbers data types/Numbers.cs
+++ Intro - Day 1/Data Types/numbers data types/Numbers.cs
@@ -3,11 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+# nullable enable
 
 namespace Intro___Day_1.Data_Types.numbers_data_types
 {
     public  class Numbers
     {
+        Decimal balance = 56m;
+        Single floatBalance = 56f;
+        bool isTrue = true;
+   public static void justRandom()
+    {
+        int sum = 3 + 4;
+            decimal firstNum = 6m;
+            decimal secondNum = 7m;
+            float floatFirstNumber = 5f;
+            float secondFirstNumber = 11f;
+            int intNum = 4;
+            secondNum = (decimal)floatFirstNumber;
+            floatFirstNumber = (float)firstNum;
+            Console.WriteLine($"{ floatFirstNumber} is of type {floatFirstNumber.GetType()}");
+            floatFirstNumber = (float)45.786m;
+            Console.WriteLine($"{ floatFirstNumber} is of type {floatFirstNumber.GetType()}");
+            float addfloatToDecimal = floatFirstNumber + intNum;
+            Console.WriteLine($"{ secondNum} is of type {secondNum.GetType()}");
+
+            bool isTrue = true;
+            bool isFalse = false;
+    }
 
     }
+    //parse
+    //ConvertTo
+
 }
--- Intro - Day 1/Program.cs
+++ Intro - Day 1/Program.cs
@@ -1,23 +1,29 @@
-﻿namespace Intro___Day_1
+﻿using Intro___Day_1.Data_Types.numbers_data_types;
+
+namespace Intro___Day_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Dog myDog = new Dog("Falon");
-            Dog yourDog = new Dog("Beauty");
-           // Console.WriteLine(myDog.Name);
-            //Console.WriteLine(yourDog.Name);
-            myDog.MakeSound();
-            yourDog.MakeSound();
+            //    Dog myDog = new Dog("Falon");
+            //    Dog yourDog = new Dog("Beauty");
+            //   // Console.WriteLine(myDog.Name);
+            //    //Console.WriteLine(yourDog.Name);
+            //    myDog.MakeSound();
+            //    yourDog.MakeSound();
 
-            
-        Cat myCat = new Cat("Betty");
-        Console.WriteLine(myCat.Newname);
-                myCat.lives();
-            GoodGames myGames = new GoodGames("Manhunt");
-            Console.WriteLine(myGames.Gamename);
-            myGames.Genre();
+
+            //Cat myCat = new Cat("Betty");
+            //Console.WriteLine(myCat.Newname);
+            //        myCat.lives();
+            //    GoodGames myGames = new GoodGames("Manhunt");
+            //    Console.WriteLine(myGames.Gamename);
+            //    myGames.Genre();
+
+
+
+            Numbers.justRandom();
         }
         
     }
